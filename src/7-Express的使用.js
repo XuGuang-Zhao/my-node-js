ea4a5f6325fceb15e21c3fc23b2aafb9862de1eb
zhao <1249632746@qq.com>
2024-03-30 22:46:50 +0800

    更新Express内容

--- src/7-Express的使用.js
+++ src/7-Express的使用.js
@@ -1,33 +1,31 @@
 const express = require('express')
+const cors = require('cors')
 const userRouter = require('./router/index')
+
 const app = express()
+
+// 配置解析表单中间件
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+
+// 配置跨域中间件
+app.use(cors())
+
+// 配置捕获异常中间键
+app.use((err, req, res, next) => {
+    console.log(err, req, res, next)
+})
+
+// 如何创建静态资源服务器
+app.use(express.static('../assets'))
+
+// 静态资源加前缀
+app.use('/assets', express.static('../assets'))
+
+// 如何注册路由模块
+app.use(userRouter)
+
 
 app.listen(80, () => {
     console.log('express server running at http://127.0.0.1')
 })
-
-// 7.定义中间件
-const mw = function(req, res, next) {
-    console.log('执行中间件')
-    next()
-}
-
-app.get('/user', mw, function(req, res) {
-    // 1. 如何发送文本内容
-    // res.send('文本内容')
-    // 2. 如何发送对象内容
-    res.send({name: 'zs', age: 20, gender: '男'})
-    // 3.如何获取请求的信息
-    console.log('req query', req.query)
-    console.log('req param', req.params)
-})
-
-// 4.如何创建静态资源服务器
-app.use(express.static('../assets'))
-
-// 5.加前缀
-app.use('/assets', express.static('../assets'))
-
-// 6. 如何注册路由模块
-app.use(userRouter)
-
