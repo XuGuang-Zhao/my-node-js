93ca427391c8d588f81a6e54bace0bf24a12429c
zhao <1249632746@qq.com>
2024-03-31 18:38:32 +0800

    兼容expressjwt版本

--- src/index.js
+++ src/index.js
@@ -20,7 +20,7 @@ app.use((req, res, next) => {
 })
 
 // 全局拦截器进行token验证
-app.use(expressJWT({secret: config.jwtSecrectKey}).unless({path: [/^\/api\//]}))
+app.use(expressJWT.expressjwt({secret: config.jwtSecrectKey, algorithms: ['HS256']}).unless({path: [/^\/api\//]}))
 
 // 全局错误拦截器添加对插件包抛出错误异常
 app.use((err, req, res, next) => {
